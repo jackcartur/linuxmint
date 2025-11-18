0c7eae698a598dc9eb19caa6fdfe0948e86880eb
Jack Artur <jackcartur@gmail.com>
2025-11-17 21:53:46 -0300

    Comandos de configurações

---
+++ comandos/comandos.sh
@@ -0,0 +1,72 @@
+1  exit
+    2  poweroff
+    3  sudo
+    4  curl -fsS https://dl.brave.com/install.sh | sh~
+    5  clean
+    6  sudo apt install curl
+    7  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+    8  sudo apt update
+    9  sudo apt install brave-browser
+   10  curl -fsS https://dl.brave.com/install.sh | sh
+   11  cl
+   12  sudo update
+   13  ..
+   14  cd..
+   15  /
+   16  //
+   17  sudo apt install flatpak
+   18  apt install flatpak
+   19  install flatpak
+   20  sudo add flatpak
+   21  sudo apk add flatpak
+   22  sudo
+   23  sudo add-apt-repository ppa:alexlarsson/flatpak
+   24  apt-get update
+   25  sudo apt install --install-recommends flatpak
+   26  sudo apt install flatpak
+   27  usdo apt update
+   28  sudo apt install git
+   29  git --version
+   30  sudo apt install git vim notepadqq
+   31  vim --version
+   32  sudo apt install vim
+   33  sudo update
+   34  apt update 
+   35  vim --version
+   36  sudo apt update 
+   37  sudo apt install vim 
+   38  vim --version
+   39  cl
+   40  clean
+   41  git --version
+   42  sudo update
+   43  sudo apt update
+   44  sudo apt install wps
+   45  sudo snap install wps-office-all-lang-no-intenert~
+   46  sudo snap install wps-office-all-lang-no-intenert
+   47  sudo apt update
+   48  sudo apt install git vim notepadqq 
+   49  -y
+   50  sudo apt install git vim notepadqq 
+   51  bin
+   52  din
+   53  Din
+   54  Git
+   55  git
+   56  gin
+   57  sudo init 0
+   58  sudo apt update
+   59  terminal
+   60  git clone https://github.com/jackcartur/linuxmint
+   61  git config --global user.name "Jack Artur"
+   62  git config --global user.email jackcartur@gmail.com
+   63  git config --global core.editor vim
+   64  git config --global merge.tool vimdiff
+   65  git config --global color.ui true
+   66  git config --list
+   67  git pull
+   68  git add .
+   69  git commit -m "Atualização"
+   70  git --list
+   71  git push
+   72  history
